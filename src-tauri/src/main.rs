674ccd9e721fb57dee6e35732245f052783aab2e
gontijolucca-prog <gontijolucca@gmail.com>
2026-05-06 14:12:36 +0100

    Fix Tauri v2 build: simplify main.rs and generate icons in workflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::WebviewUrl;
-
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let _window = tauri::window::WindowBuilder::new(
-                app,
-                "main",
-                WebviewUrl::App("https://gontijolucca-prog.github.io/calculadoraempresaouindividual/".parse().unwrap()),
-            )
-            .title("Recofátima Simuladores")
-            .inner_size(1280.0, 800.0)
-            .resizable(true)
-            .build()?;
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
